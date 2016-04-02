653569a28b4dc5367debbb00a53e27bbaf70ddf1
Jonas Coch <jonas@iosphere.de>
2016-04-02 22:01:34 +0200

    Remove unnecessary wrapper code

--- src/scripts/Web.elm
+++ src/scripts/Web.elm
@@ -1,19 +1,19 @@
 module Web (..) where
 
-import Effects as Fx exposing (Effects)
-import Html exposing (..)
+import Effects exposing (Effects)
+import Html exposing (Html)
 import StartApp
-import Task
+import Task exposing (Task)
 import Component.Search as Search
 
 
-app : StartApp.App Model
+app : StartApp.App Search.Model
 app =
   StartApp.start
-    { init = init
-    , view = view
-    , update = update
-    , inputs = [ Signal.map UpdateSearch Search.querySignal ]
+    { init = Search.init
+    , view = Search.view
+    , update = Search.update
+    , inputs = [ Search.querySignal ]
     }
 
 
@@ -22,60 +22,6 @@ main =
   app.html
 
 
-port worker : Signal (Task.Task Fx.Never ())
+port worker : Signal (Task Effects.Never ())
 port worker =
   app.tasks
-
-
-
--- MODEL
-
-
-type alias Model =
-  { search : Search.Model
-  }
-
-
-
--- INIT
-
-
-init : ( Model, Effects Action )
-init =
-  let
-    ( search, searchFx ) =
-      Search.init
-  in
-    ( Model search
-    , Fx.map UpdateSearch searchFx
-    )
-
-
-
--- UPDATE
-
-
-type Action
-  = UpdateSearch Search.Action
-
-
-update : Action -> Model -> ( Model, Effects Action )
-update action model =
-  case action of
-    UpdateSearch act ->
-      let
-        ( newSearch, fx ) =
-          Search.update act model.search
-      in
-        ( { model | search = newSearch }
-        , Fx.map UpdateSearch fx
-        )
-
-
-
--- VIEW
-
-
-view : Signal.Address Action -> Model -> Html
-view addr model =
-  Search.view (Signal.forwardTo addr UpdateSearch) model.search
