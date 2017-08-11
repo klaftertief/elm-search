7382a7cfa8e301e712ea86becfb6dd2a9e44a425
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-08-11 13:48:59 -0500

    Move HTML into View

--- src/scripts/Docs/Type.elm
+++ src/scripts/Docs/Type.elm
@@ -1,23 +1,18 @@
 module Docs.Type
     exposing
-        ( Context(..)
-        , Type(..)
+        ( Type(..)
         , decoder
-        , length
         , normalize
         , parse
         , reserverdVars
-        , toHtml
         )
 
 import Char
 import Dict exposing (Dict)
 import Docs.Name as Name exposing (Name)
 import Elm.Documentation.Type as Type
-import Html exposing (..)
 import Json.Decode as Decode exposing (Decoder)
 import String
-import Utils.Code as Code exposing (arrow, colon, padded, space)
 import Utils.Json
 
 
@@ -27,153 +22,6 @@ type Type
     | Apply Name (List Type)
     | Tuple (List Type)
     | Record (List ( String, Type )) (Maybe String)
-
-
-type Context
-    = Func
-    | App
-    | Other
-
-
-toHtml : Context -> Type -> List (Html msg)
-toHtml context tipe =
-    case tipe of
-        Function args result ->
-            let
-                maybeAddParens =
-                    case context of
-                        Func ->
-                            Code.addParens
-
-                        App ->
-                            Code.addParens
-
-                        Other ->
-                            identity
-
-                argsHtml =
-                    List.concatMap (\arg -> toHtml Func arg ++ padded arrow) args
-            in
-            maybeAddParens (argsHtml ++ toHtml Func result)
-
-        Var name ->
-            [ text name ]
-
-        Apply name [] ->
-            [ text name.name ]
-
-        Apply name args ->
-            let
-                maybeAddParens =
-                    case context of
-                        Func ->
-                            identity
-
-                        App ->
-                            Code.addParens
-
-                        Other ->
-                            identity
-
-                argsHtml =
-                    List.concatMap (\arg -> space :: toHtml App arg) args
-            in
-            maybeAddParens (text name.name :: argsHtml)
-
-        Tuple args ->
-            List.map (toHtml Other) args
-                |> List.intersperse [ text ", " ]
-                |> List.concat
-                |> Code.addParens
-
-        Record fields ext ->
-            let
-                fieldsHtml =
-                    List.map fieldToHtml fields
-                        |> List.intersperse [ text ", " ]
-                        |> List.concat
-
-                recordInsides =
-                    case ext of
-                        Nothing ->
-                            fieldsHtml
-
-                        Just extName ->
-                            text extName :: text " | " :: fieldsHtml
-            in
-            text "{ " :: recordInsides ++ [ text " }" ]
-
-
-fieldToHtml : ( String, Type ) -> List (Html msg)
-fieldToHtml ( field, tipe ) =
-    text field :: space :: colon :: space :: toHtml Other tipe
-
-
-length : Context -> Type -> Int
-length context tipe =
-    case tipe of
-        Function args result ->
-            let
-                parens =
-                    case context of
-                        Func ->
-                            2
-
-                        App ->
-                            2
-
-                        Other ->
-                            0
-
-                argLengths =
-                    List.map (\t -> 4 + length Func t) args
-            in
-            parens + List.sum argLengths + length Func result
-
-        Var name ->
-            String.length name
-
-        Apply { name } [] ->
-            String.length name
-
-        Apply { name } args ->
-            let
-                parens =
-                    case context of
-                        Func ->
-                            0
-
-                        App ->
-                            2
-
-                        Other ->
-                            0
-
-                argsLength =
-                    List.sum (List.map (\t -> 1 + length App t) args)
-            in
-            parens + String.length name + argsLength
-
-        Tuple args ->
-            List.sum (List.map (\t -> 2 + length Other t) args)
-
-        Record fields ext ->
-            let
-                fieldLength ( field, tipe ) =
-                    String.length field + 3 + length Other tipe
-
-                recordLength =
-                    2 + List.sum (List.map (\ft -> 2 + fieldLength ft) fields)
-
-                extLength =
-                    case ext of
-                        Nothing ->
-                            0
-
-                        Just extName ->
-                            2 + String.length extName
-            in
-            recordLength + extLength
 
 
 parse : String -> Result String Type
--- src/scripts/Search/View.elm
+++ src/scripts/Search/View.elm
@@ -8,7 +8,7 @@ import Logo
 import Search.Chunk as Chunk exposing (Chunk)
 import Search.Model as Model exposing (..)
 import String
-import Utils.Code exposing (arrow, colon, equals, keyword, padded, space)
+import Utils.Code as Code
 import Utils.Markdown as Markdown
 
 
@@ -154,13 +154,13 @@ annotation : Chunk -> List (List (Html msg))
 annotation chunk =
     case chunk.tipe of
         Type.Function args result ->
-            if String.length chunk.context.name + 3 + Type.length Type.Other chunk.tipe > 64 then
+            if String.length chunk.context.name + 3 + typeLength Other chunk.tipe > 64 then
                 [ annotationName chunk ] :: longFunctionAnnotation args result
             else
-                [ annotationName chunk :: padded colon ++ Type.toHtml Type.Other chunk.tipe ]
+                [ annotationName chunk :: Code.padded Code.colon ++ viewType Other chunk.tipe ]
 
         _ ->
-            [ annotationName chunk :: padded colon ++ Type.toHtml Type.Other chunk.tipe ]
+            [ annotationName chunk :: Code.padded Code.colon ++ viewType Other chunk.tipe ]
 
 
 annotationName : Chunk -> Html msg
@@ -173,10 +173,161 @@ longFunctionAnnotation : List Type -> Type -> List (List (Html msg))
 longFunctionAnnotation args result =
     let
         tipeHtml =
-            List.map (Type.toHtml Type.Func) (args ++ [ result ])
+            List.map (viewType Func) (args ++ [ result ])
 
         starters =
-            [ text "    ", colon, text "  " ]
-                :: List.repeat (List.length args) [ text "    ", arrow, space ]
+            [ text "    ", Code.colon, text "  " ]
+                :: List.repeat (List.length args) [ text "    ", Code.arrow, Code.space ]
     in
     List.map2 (++) starters tipeHtml
+
+
+
+-- TYPE
+
+
+type Context
+    = Func
+    | App
+    | Other
+
+
+viewType : Context -> Type -> List (Html msg)
+viewType context tipe =
+    case tipe of
+        Type.Function args result ->
+            let
+                maybeAddParens =
+                    case context of
+                        Func ->
+                            Code.addParens
+
+                        App ->
+                            Code.addParens
+
+                        Other ->
+                            identity
+
+                argsHtml =
+                    List.concatMap (\arg -> viewType Func arg ++ Code.padded Code.arrow) args
+            in
+            maybeAddParens (argsHtml ++ viewType Func result)
+
+        Type.Var name ->
+            [ text name ]
+
+        Type.Apply name [] ->
+            [ text name.name ]
+
+        Type.Apply name args ->
+            let
+                maybeAddParens =
+                    case context of
+                        Func ->
+                            identity
+
+                        App ->
+                            Code.addParens
+
+                        Other ->
+                            identity
+
+                argsHtml =
+                    List.concatMap (\arg -> Code.space :: viewType App arg) args
+            in
+            maybeAddParens (text name.name :: argsHtml)
+
+        Type.Tuple args ->
+            List.map (viewType Other) args
+                |> List.intersperse [ text ", " ]
+                |> List.concat
+                |> Code.addParens
+
+        Type.Record fields ext ->
+            let
+                fieldsHtml =
+                    List.map viewField fields
+                        |> List.intersperse [ text ", " ]
+                        |> List.concat
+
+                recordInsides =
+                    case ext of
+                        Nothing ->
+                            fieldsHtml
+
+                        Just extName ->
+                            text extName :: text " | " :: fieldsHtml
+            in
+            text "{ " :: recordInsides ++ [ text " }" ]
+
+
+viewField : ( String, Type ) -> List (Html msg)
+viewField ( field, tipe ) =
+    text field :: Code.space :: Code.colon :: Code.space :: viewType Other tipe
+
+
+typeLength : Context -> Type -> Int
+typeLength context tipe =
+    case tipe of
+        Type.Function args result ->
+            let
+                parens =
+                    case context of
+                        Func ->
+                            2
+
+                        App ->
+                            2
+
+                        Other ->
+                            0
+
+                argLengths =
+                    List.map (\t -> 4 + typeLength Func t) args
+            in
+            parens + List.sum argLengths + typeLength Func result
+
+        Type.Var name ->
+            String.length name
+
+        Type.Apply { name } [] ->
+            String.length name
+
+        Type.Apply { name } args ->
+            let
+                parens =
+                    case context of
+                        Func ->
+                            0
+
+                        App ->
+                            2
+
+                        Other ->
+                            0
+
+                argsLength =
+                    List.sum (List.map (\t -> 1 + typeLength App t) args)
+            in
+            parens + String.length name + argsLength
+
+        Type.Tuple args ->
+            List.sum (List.map (\t -> 2 + typeLength Other t) args)
+
+        Type.Record fields ext ->
+            let
+                fieldLength ( field, tipe ) =
+                    String.length field + 3 + typeLength Other tipe
+
+                recordLength =
+                    2 + List.sum (List.map (\ft -> 2 + fieldLength ft) fields)
+
+                extLength =
+                    case ext of
+                        Nothing ->
+                            0
+
+                        Just extName ->
+                            2 + String.length extName
+            in
+            recordLength + extLength
