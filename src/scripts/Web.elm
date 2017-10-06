b229c147f11e6f7b8b6ed5073daa2e9195cdaeb5
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-10-05 19:40:46 -0500

    Combine Web app files

--- src/scripts/Web.elm
+++ src/scripts/Web.elm
@@ -1,20 +1,147 @@
-module Web exposing (..)
+port module Web exposing (program)
 
+import Dict
 import Docs.Package exposing (Package)
-import Html
-import Ports
-import Web.Model as Search exposing (..)
-import Web.Update as Search
-import Web.View as Search
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import Http
+import Search.Model as Search
+import Search.Update as Search
+import Search.View as Search
 
 
 program : List Package -> Program Flags Model Msg
 program packages =
     Html.programWithFlags
-        { init = Search.init packages
-        , view = Search.view
-        , update = Search.update
-        , subscriptions =
-            \_ ->
-                Sub.batch [ Ports.query LocationSearchChange ]
+        { init = init packages
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
         }
+
+
+type Model
+    = Ready Search.Model
+
+
+type Msg
+    = SearchMsg Search.Msg
+    | LocationSearchChange String
+
+
+type alias Flags =
+    { index : String
+    , search : String
+    }
+
+
+toQueryString : Search.Filter -> String
+toQueryString { queryString } =
+    if String.isEmpty queryString then
+        ""
+    else
+        "?q=" ++ Http.encodeUri queryString
+
+
+decodeQuery : String -> String
+decodeQuery query =
+    String.join "%20" (String.split "+" query)
+
+
+parseSearchString : String -> Search.Filter
+parseSearchString searchString =
+    case String.uncons (decodeQuery searchString) of
+        Just ( '?', rest ) ->
+            let
+                parts =
+                    String.split "&" rest
+                        |> List.map (String.split "=")
+                        |> List.filterMap decodePair
+                        |> Dict.fromList
+
+                queryString =
+                    Dict.get "q" parts
+                        |> Maybe.withDefault ""
+
+                query =
+                    Search.queryListFromString queryString
+            in
+            { queryString = queryString
+            , query = query
+            , lastQuery = ""
+            }
+
+        _ ->
+            Search.initialFilter
+
+
+decodePair : List String -> Maybe ( String, String )
+decodePair pair =
+    case pair of
+        [ k, v ] ->
+            Maybe.map2 (,) (Http.decodeUri k) (Http.decodeUri v)
+
+        _ ->
+            Nothing
+
+
+init : List Package -> Flags -> ( Model, Cmd Msg )
+init packages { index, search } =
+    let
+        searchModel =
+            Search.init (parseSearchString search) packages
+    in
+    ( Ready searchModel, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg (Ready search) =
+    case msg of
+        SearchMsg Search.RunFilter ->
+            let
+                newSearch =
+                    Search.update Search.RunFilter search
+            in
+            ( Ready newSearch, toQueryString search.filter |> pushQuery )
+
+        SearchMsg searchMsg ->
+            let
+                newSearch =
+                    Search.update searchMsg search
+            in
+            ( Ready newSearch, Cmd.none )
+
+        LocationSearchChange queryString ->
+            let
+                filter =
+                    parseSearchString queryString
+
+                newSearch =
+                    if filter /= search.filter then
+                        search
+                            |> Search.update (Search.SetFilter filter)
+                            |> Search.update Search.RunFilter
+                    else
+                        search
+            in
+            ( Ready newSearch, Cmd.none )
+
+
+view : Model -> Html Msg
+view (Ready search) =
+    Html.map SearchMsg <|
+        div [ class "searchReady" ]
+            [ Search.viewSearchHeader search
+            , Search.viewSearchBody search
+            ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    query LocationSearchChange
+
+
+port query : (String -> msg) -> Sub msg
+
+
+port pushQuery : String -> Cmd msg
