3195ab6e146f988a1a7c75b6bb88ab0bb39a6b10
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-10-09 16:11:00 -0500

    Generate elm code

--- src/scripts/Generate.elm
+++ src/scripts/Generate.elm
@@ -1,6 +1,7 @@
 module Generate exposing (elm)
 
-import Docs.Package exposing (Package)
+import Docs.Package exposing (Entry, Module, Package)
+import Docs.Type exposing (Type(..))
 
 
 elm : List Package -> String
@@ -9,27 +10,132 @@ elm packages =
         [ "module Main exposing (..)"
         , ""
         , "import Web"
-        , "import Docs.Type"
+        , "import Docs.Type exposing (Type(..))"
         , ""
         , "main ="
-        , "  Web.program <|"
-        , "    " ++ generate packages
-        , ""
+        , "  Web.program"
+        , "    [ " ++ String.join "\n    , " (List.map fromPackage packages)
+        , "    ]"
         ]
 
 
-generate : List Package -> String
-generate =
-    chunk toString []
-        >> List.map toString
-        >> String.join " ++ "
+fromPackage : Package -> String
+fromPackage { user, name, version, modules } =
+    record
+        [ ( "user", string user )
+        , ( "name", string name )
+        , ( "version", string version )
+        , ( "modules", list fromModule modules )
+        ]
 
 
-chunk : (a -> b) -> List (List b) -> List a -> List (List b)
-chunk fn acc original =
-    case original of
-        a :: b :: c :: d :: e :: f :: rest ->
-            chunk fn ([ fn a, fn b, fn c, fn d, fn e, fn f ] :: acc) rest
+fromModule : Module -> String
+fromModule { name, entries, elmVersion } =
+    record
+        [ ( "name", string name )
+        , ( "elmVersion", maybe string elmVersion )
+        , ( "entries", list fromEntry entries )
+        ]
 
-        _ ->
-            List.map fn original :: acc
+
+fromEntry : Entry -> String
+fromEntry { name, docs, tipe } =
+    record
+        [ ( "name", string name )
+        , ( "docs", string docs )
+        , ( "tipe", fromType tipe )
+        ]
+
+
+fromType : Type -> String
+fromType tipe =
+    withParens <|
+        case tipe of
+            Function args last ->
+                "Function "
+                    ++ list fromType args
+                    ++ " "
+                    ++ withParens (fromType last)
+
+            Var name ->
+                "Var " ++ string name
+
+            Apply { home, name } args ->
+                "Apply "
+                    ++ record
+                        [ ( "name", string name )
+                        , ( "home", string home )
+                        ]
+                    ++ " "
+                    ++ list fromType args
+
+            Tuple args ->
+                "Tuple "
+                    ++ list fromType args
+
+            Record pairs extensible ->
+                "Record "
+                    ++ list (tuple string fromType) pairs
+                    ++ " "
+                    ++ withParens (maybe string extensible)
+
+
+withParens : String -> String
+withParens value =
+    "(" ++ value ++ ")"
+
+
+tuple : (a -> String) -> (b -> String) -> ( a, b ) -> String
+tuple f g ( a, b ) =
+    withParens <| f a ++ ", " ++ g b
+
+
+string : String -> String
+string value =
+    "\"" ++ String.foldl stringEscape "" value ++ "\""
+
+
+stringEscape : Char -> String -> String
+stringEscape next acc =
+    String.append acc <|
+        case next of
+            '"' ->
+                "\\\""
+
+            '\\' ->
+                "\\\\"
+
+            '\n' ->
+                "\\n"
+
+            '\t' ->
+                "\\t"
+
+            '\x0D' ->
+                "\\r"
+
+            _ ->
+                String.fromChar next
+
+
+maybe : (a -> String) -> Maybe a -> String
+maybe f =
+    Maybe.map (\value -> "Just " ++ f value)
+        >> Maybe.withDefault "Nothing"
+
+
+list : (a -> String) -> List a -> String
+list f values =
+    "[" ++ String.join ", " (List.map f values) ++ "]"
+
+
+record : List ( String, String ) -> String
+record fields =
+    "{ "
+        ++ String.join ", " (List.map recordField fields)
+        ++ " }"
+
+
+recordField : ( String, String ) -> String
+recordField ( key, value ) =
+    key ++ " = " ++ value
