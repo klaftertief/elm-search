aee875babe6bb66f94565b855ce7bf1bf0c67481
klaftertief <jonas.coch@itravel.de>
2019-04-14 21:53:27 +0200

    Limit number of search results

--- src/Backend.elm
+++ src/Backend.elm
@@ -75,6 +75,7 @@ update msg model =
                       , Search.search (Query.fromString queryString |> Debug.log "q") model.index
                             |> List.filter (Tuple.first >> (\d -> d < 0.2))
                             |> List.sortBy Tuple.first
+                            |> List.take 20
                             |> Json.Encode.list (Tuple.second >> Index.encodeBlock)
                       )
                     ]
