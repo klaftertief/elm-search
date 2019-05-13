fd61b66d6c2c3647fb1b0de0a1ebf0570906340d
klaftertief <jonas.coch@itravel.de>
2019-05-13 23:50:08 +0200

    Start implementing partial distance

--- src/Elm/Type/Partial/Distance.elm
+++ src/Elm/Type/Partial/Distance.elm
@@ -3,24 +3,89 @@ module Elm.Type.Partial.Distance exposing (distance)
 import Dict exposing (Dict)
 import Elm.Type as Type exposing (Type)
 import Elm.Type.Distance as Distance
-import Elm.Type.Partial as Partial exposing (Partial)
+import Elm.Type.Partial as Partial
 import List.Extra
 
 
-distance : Partial -> Type -> Float
+distance : Partial.Type -> Type -> Float
 distance from to =
     case ( from, to ) of
+        -- PARTIALS
         ( Partial.LambdaFrom partialFrom, Type.Lambda lambdaFrom _ ) ->
-            Distance.distance partialFrom lambdaFrom
+            distance partialFrom lambdaFrom
+
+        ( Partial.Lambda partialFrom (Partial.Unknown ""), Type.Lambda lambdaFrom _ ) ->
+            distance partialFrom lambdaFrom
 
         ( Partial.LambdaTo partialTo, Type.Lambda _ lambdaTo ) ->
-            Distance.distance partialTo lambdaTo
+            distance partialTo lambdaTo
 
         ( Partial.VariableType _ variableTypeArgs, Type.Type _ typeArgs ) ->
-            Distance.distanceList variableTypeArgs typeArgs
+            distanceList variableTypeArgs typeArgs
+
+        -- NORMALS
+        ( Partial.Lambda from1 to1, Type.Lambda from2 to2 ) ->
+            (distance from1 from2 + distance to1 to2) / 2
+
+        ( Partial.Var fromName, Type.Var toName ) ->
+            Distance.varDistance fromName toName
+
+        ( Partial.Var varName, Type.Type "Maybe.Maybe" [ Type.Var wrappedVarName ] ) ->
+            Distance.varDistance varName wrappedVarName
+
+        ( Partial.Type "Maybe" [ Partial.Var wrappedVarName ], Type.Var varName ) ->
+            Distance.varDistance varName wrappedVarName
+
+        ( Partial.TypeQual "Maybe" "Maybe" [ Partial.Var wrappedVarName ], Type.Var varName ) ->
+            Distance.varDistance varName wrappedVarName
+
+        ( Partial.Var varName, Type.Type "Result.Result" [ Type.Var wrappedVarName ] ) ->
+            Distance.varDistance varName wrappedVarName
+
+        ( Partial.Type "Result" [ Partial.Var wrappedVarName ], Type.Var varName ) ->
+            Distance.varDistance varName wrappedVarName
+
+        ( Partial.TypeQual "Result" "Result" [ Partial.Var wrappedVarName ], Type.Var varName ) ->
+            Distance.varDistance varName wrappedVarName
+
+        ( Partial.Var varName, Type.Type typeName typeArgs ) ->
+            Distance.varTypeDistance varName ( typeName, typeArgs )
+
+        -- ( Partial.Type typeName typeArgs, Type.Var varName ) ->
+        --     Distance.varTypeDistance varName ( typeName, typeArgs )
+        -- ( Partial.TypeQual typeQual typeName typeArgs, Type.Var varName ) ->
+        --     Distance.varTypeDistance varName ( typeQual ++ "." ++ typeName, typeArgs )
+        -- ( Partial.Type fromName fromArgs, Type.Type toName toArgs ) ->
+        --     distanceApply ( canonicalN, argsN ) ( canonicalH, argsH )
+        ( Partial.Pair fromFirst fromSecond, Type.Tuple [ toFirst, toSecond ] ) ->
+            distanceList [ fromFirst, fromSecond ] [ toFirst, toSecond ]
+
+        ( Partial.Triple fromFirst fromSecond fromThird, Type.Tuple [ toFirst, toSecond, toThird ] ) ->
+            distanceList [ fromFirst, fromSecond, fromThird ] [ toFirst, toSecond, toThird ]
 
         _ ->
             maxPenalty
+
+
+distanceList : List Partial.Type -> List Type -> Float
+distanceList from to =
+    let
+        fromLength =
+            List.length from
+    in
+    if fromLength /= List.length to then
+        maxPenalty
+
+    else
+        List.Extra.permutations from
+            |> List.map
+                (\currentFrom ->
+                    List.map2 distance currentFrom to
+                        |> List.sum
+                        |> (\a -> (/) a (toFloat fromLength))
+                )
+            |> List.minimum
+            |> Maybe.withDefault maxPenalty
 
 
 noPenalty : Float
